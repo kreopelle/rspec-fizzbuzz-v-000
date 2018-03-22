b596b406069f07c3342a48f9912668d41243432d
Kayla Reopelle <kreopelle@gmail.com>
2018-03-22 02:25:26 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,11 @@
-def fizzbuzz
-
+def fizzbuzz(int)
+  if (int % 3 == 0) && (int % 5 == 0)
+    puts "FizzBuzz"
+  elsif (int % 3 == 0)
+    puts "Fizz"
+  elsif (int % 5 == 0)
+    puts "Buzz"
+  else
+    nil
+  end
 end
